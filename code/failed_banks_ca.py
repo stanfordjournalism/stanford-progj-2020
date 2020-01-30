3cfb95999fa0d7564da424dae466fe6f2dd5e206
Serdar Tumgoren <zstumgoren@gmail.com>
2020-01-29 17:02:52 -0800

    update TODOs in failed_banks_ca.py

--- code/failed_banks_ca.py
+++ code/failed_banks_ca.py
@@ -1,10 +1,11 @@
 """
 This script should download the list of FDIC failed
 banks and generate a new file containing
-just the CA banks. It should also prints out
+just the CA banks. It should also print out
 the number of failed banks in CA.
 
-See the TODO notes below for details on each step.
+See the TODO notes below for detailed requirements
+on each step of the code.
 
 USAGE:
 
@@ -16,31 +17,28 @@ USAGE:
 # URL TO THE FDIC BANKS DATA
 url = 'https://www.fdic.gov/bank/individual/failed/banklist.csv'
 
-# TODO: Use requests to fetch the FDIC data
+# TODO: Use the requests library to fetch the FDIC data
 
 
 # TODO: Use the "with" statement and "open" function,
-# along with the CSV module, to write file
-# contents (returned by requests)
-# to a local file caled "banklist.csv"
+# along with the CSV module, to write file contents
+# (as returned by requests) to a local file caled "banklist.csv"
 
 
-# TODO: Use "with" and "open" to read the CSV module
+# TODO: Use "with" and "open" along with the CSV module
 # to read the local "banklist.csv"
 
-
-# TODO: Perform the following actions on each row of "banklist.csv"
-# - filter the data for just California banks
-# - reformat the Closing Date value in each row to YYYY-MM-DD format
-#   using the datetime module's strptime and strftime functions
-# - Save the updated data (so you can create a new file below)
+    # TODO: Perform the following actions on each row of "banklist.csv"
+    # - filter the data for just California banks
+    # - reformat the Closing Date value in each row to YYYY-MM-DD format
+    #   using the datetime module's strptime and strftime functions
+    # - Save the updated data (so you can create a new file below)
 
 
 # TODO: Use the "with" statement and "open" function,
-# along with the CSV module, to write a new file called failed_banks_ca.csv
-# The new file must contain the same header row as the original file!
+# along with the CSV module, to write a new file called "failed_banks_ca.csv"
+# The new file must contain the same header row and columns as the original file!
 
 
 # TODO: print the sentence "There are X failed banks in CA.",
-# making sure to replace the X dynamically with a count of the
-# CA banks.
+# replacing the X dynamically with a count of the CA banks.
