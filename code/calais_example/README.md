1a876b49b5c97b2d643ec88c48b78936d88e1543
Serdar Tumgoren <zstumgoren@gmail.com>
2020-02-11 10:36:21 -0800

    Initial pass at OpenCalais demo

---
+++ code/calais_example/README.md
@@ -0,0 +1,44 @@
+# OpenCalais Entity Extraction
+
+## Overview
+
+The [Python script in this directory](calais_example.py) demonstrates how to call the [OpenCalais API][] to perform [entity extraction][]. The example uses a paragraph of text from a 2020 FDA medical device [recall announcement][].
+
+
+## Obtain an API Key
+
+Before using this code, you must [register for an API key][]. 
+
+Once you've registered for an API key, log in to your new Reuters/Refinitiv account at <https://developers.refinitiv.com/>.
+
+Once you're logged in, you can obtain your API key by clicking on the user settings in the upper right and selecting `My Keys`.
+
+![Locate OpenCalais API Key](../../static/opencalais_get_api_key.png)
+
+Copy the long string of characters next to `Your Key`.
+
+Next, create the below shell environment variables by [exporting them](../../docs/using_env_vars_for_secrets.md) in `~/.bash_profile` and *substituting your information*:
+
+> Note, use the email that you registered with for the OpenCalais service.
+
+```
+# ~/.bash_profile
+export OPENCALAIS_API_USER="USERNAME@email_address.com"
+export OPENCALAIS_API_KEY="YOUR_API_KEY"
+```
+
+The script is a simplified version of this [code](http://www.opencalais.com/python-code-sample/).
+
+## Additional Resources
+
+* [OpenCalais Demo][]
+* [A Practical Approach to Understanding and Ingesting TRIT Output for Your Use Case][]
+
+
+[OpenCalais API]: https://developers.refinitiv.com/open-permid/intelligent-tagging-restful-api
+[entity extraction]: https://en.wikipedia.org/wiki/Named-entity_recognition
+[exporting them]: ../../docs/python/using_env_vars_for_secrets.md
+[OpenCalais Demo]: https://permid.org/onecalaisViewer
+[recall announcement]: https://www.fda.gov/MedicalDevices/Safety/ListofRecalls/ucm630614.htm
+[register for an API key]: https://iamui.thomsonreuters.com/iamui/UI/createUser?app_id=Bold&realm=Bold&realm=Bold
+[A Practical Approach to Understanding and Ingesting TRIT Output for Your Use Case]: https://developers.thomsonreuters.com/article/practical-approach-understanding-and-ingesting-trit-output-your-use-case
---
+++ code/calais_example/calais_example.py
@@ -0,0 +1,47 @@
+"""
+Example Python script showing how to call the
+OpenCalais Intelligent Tagging API.
+
+See readme.md alongside this script for more details.
+"""
+import os
+import json
+import requests
+
+
+#CALAIS_URL = 'https://api.thomsonreuters.com/permid/calais'
+CALAIS_URL = 'https://api-eit.refinitiv.com/permid/calais'
+# BELOW VARIABLES MUST BE SET IN ~/.bash_profile
+# See here for more background:
+# https://github.com/stanfordjournalism/stanford-progj-2020/blob/master/code/using_env_vars_for_secrets.md
+USER = os.environ['OPENCALAIS_API_USER']
+API_KEY = os.environ['OPENCALAIS_API_KEY']
+
+EXAMPLE_TEXT= """
+    Smiths Medical is recalling the sterile saline and sterile water products for inhalation due to the potential exposure to infectious agents (bacillus infantis and staphylococcus epidermidis) because of damage to the containers used to package the finished products. Use of these products in patients could result in infection and may require treatment with antibiotics. Serious or untreated infections could result in patient death.
+
+The company initiated a voluntary recall on September 5, 2017. That recall covered several products, including some that are outside the scope of this notice. The FDA is auditing the recall to ensure the company has notified all affected customers and that affected product has been returned. Based on the available information, the FDA is now classifying the action regarding the affected products (listed below) as a Class I recall.
+"""
+
+def main():
+    headers = {
+        'X-AG-Access-Token' : API_KEY,
+        'Content-Type' : 'text/raw',
+        'outputformat' : 'application/json',
+        'x-calais-selectiveTags': 'company,industry,socialtags'
+    }
+    # Construst a POST request, as required by the OpenCalais API
+    response = requests.post(
+        CALAIS_URL,
+        data=EXAMPLE_TEXT,
+        headers=headers,
+        timeout=80
+    )
+    outfile = '/tmp/recall_example.json'
+    with open(outfile,'w') as out:
+        if response.status_code == 200:
+            print("Writing response to {}".format(outfile))
+            json.dump(response.json(), out, indent=4)
+
+if __name__ == '__main__':
+    main()
